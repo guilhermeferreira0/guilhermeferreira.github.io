15dbe221d08376fd48403dc43a73816c8786ec13
Guilherme Ferreira <guilherme.fsilva23@hotmail.com>
2024-07-29 11:18:42 -0300

    Starting project

---
+++ src/app/layout.tsx
@@ -0,0 +1,38 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Mono, JetBrains_Mono } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: '--font-inter'
+});
+
+const plexMono = IBM_Plex_Mono({
+  variable: '--font-plex-mono',
+  subsets: ["latin"],
+  weight: ['400', '500']
+});
+
+const jetBrains = JetBrains_Mono({
+  subsets: ["latin"],
+  variable: '--font-jetbrains'
+});
+
+export const metadata: Metadata = {
+  title: "FerreiraCodes",
+  description: "Welcome to my portofolio page",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={`${inter.variable} ${plexMono.variable} ${jetBrains.variable}`}>
+        {children}
+        </body>
+    </html>
+  );
+}
---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <div className="container">
+
+      </div>
+    </header>
+  );
+}
